8b251600d760d1f156b504b80c182b666e6d4e49
jenkins <jenkins@kurento.org>
2016-03-10 13:31:02 +0000

    Update fiware_readthedocs style configuration

--- conf.py
+++ conf.py
@@ -7,8 +7,18 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme_path = ['themes']
     html_theme = 'sphinx_rtd_theme'
+    html_context = {
+      'css_files': [
+          'https://fiware.org/style/fiware_readthedocs.css'
+      ]
+    }
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
+# Using downloaded sphinx_rtd_theme
+# import sphinx_rtd_theme
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 project = 'FIWARE-Stream-Oriented-GE'
 html_style = 'css/kurento.css'
